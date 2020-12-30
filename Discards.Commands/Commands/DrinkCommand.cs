dc3018353b89d72222c24a275481e6f0b78813ca
John Nguyen <jnguyen@korterra.com>
2020-12-29 18:54:28 -0600

    Drink Api

---
+++ Discards.Commands/Commands/DrinkCommand.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Discards.Shared.Extensions;
+
+using Discord.Commands;
+
+namespace Discards.Commands.Commands
+{
+	public class DrinkCommand : ModuleBase
+	{
+		public static List<string> users = new List<string>().Shuffle();
+
+		[Command("Drink")]
+		public async Task RandomDrink()
+		{
+			var msg = $"{users.First()} Drink!";
+			await ReplyAsync(msg);
+		}
+
+		[Command("Drink Add")]
+		public async Task Add(string name)
+		{
+			users.Add(name);
+			await ReplyAsync($"{name} Added");
+		}
+		[Command("Drink All")]
+		public async Task All()
+		{
+			foreach (var user in users)
+			{
+				var msg = $"{user} Drink!";
+				await ReplyAsync(msg);
+			}
+		}
+
+		[Command("Drink Remove")]
+		public async Task Remove(string name)
+		{
+			users.Remove(name);
+			await ReplyAsync($"{name} Removed");
+		}
+
+		[Command("Drink Show")]
+		public async Task Show()
+		{
+			var msg = users.ToJsonString();
+			await ReplyAsync(msg);
+		}
+	}
+
+}
